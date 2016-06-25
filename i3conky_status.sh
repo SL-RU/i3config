f7350afaa3716525f58a892885dd169ce304688a
Lyra Heartstrings <lyra@deb-june09-z510.lan>
2016-06-25 11:36:22 +0300

    maaany chngs

--- i3conky_status.sh
+++ i3conky_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Send the header so that i3bar knows we want to use JSON:
-echo "{ \"version\": 1, \"stop_signal\": 10, \"cont_signal\": 12, \"click_events\": true }"
+echo "{ \"version\": 1}"
 
 # Begin the endless array.
 echo '['
@@ -10,15 +10,15 @@ echo '['
 echo '[],'
 
 # Now send blocks with information forever:
-conky -d -c $HOME/.i3/conky.conf
+conky -c $HOME/.i3/conky.conf
 
 
-IFS="}"
-while read;do
-  IFS=" "
-  STR=`echo $REPLY | sed -e s/[{}]//g -e "s/ \"/\"/g" | awk '{n=split($0,a,","); for (i=1; i<=n; i++) {m=split(a[i],b,":"); if (b[1] == "\"name\"") {NAME=b[2]} if (b[1] =="\"x\"") {X=b[2]} if (b[1] =="\"y\"") {Y=b[2]} } print NAME " " X " " Y}'`
-  read NAME X Y <<< $STR
-  X=$(($X-50))
+#IFS="}"
+#while read;do
+#  IFS=" "
+#  STR=`echo $REPLY | sed -e s/[{}]//g -e "s/ \"/\"/g" | awk '{n=split($0,a,","); for (i=1; i<=n; i++) {m=split(a[i],b,":"); if (b[1] == "\"name\"") {NAME=b[2]} if (b[1] =="\"x\"") {X=b[2]} if (b[1] =="\"y\"") {Y=b[2]} } print NAME " " X " " Y}'`
+#  read NAME X Y <<< $STR
+#  X=$(($X-50))
 #  case "${NAME}" in
 #      \"yad\")
 #	  notify-send "$(yandex-disk status)"
@@ -29,5 +29,5 @@ while read;do
 #    *)
 #      ;;
 #  esac
-  IFS="}"
-done
+#  IFS="}"
+#done
---
+++ resolution.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xrandr --output eDP1 --primary --auto --output VGA1 --pos 0x0 --auto --left-of eDP1 --output HDMI1 --auto --right-of eDP1
