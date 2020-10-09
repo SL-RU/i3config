d731ac5aef90656fa937e179c92023aebab42c21
Alexander Lutsai <s.lyra@ya.ru>
2020-10-09 23:27:51 +0300

    upd

--- resolution.sh
+++ resolution.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xrandr --output eDP1 --primary --auto --output VGA1 --pos 0x0 --auto --left-of eDP1 --output HDMI1 --auto --right-of eDP1
+xrandr --output HDMI-2 --auto --primary --pos 0x0 --output VGA-1 --auto --pos 0x0 --output LVDS-1 --auto --right-of HDMI-2
