5c1708110377af2c04319c1ade2377119d8fcddc
Lutsay Aleksandr Valeryevich <alutsay@ozon.ru>
2024-04-28 23:20:22 +0300

    fix screen idle, use normcap and flameshot and etc

--- s.sh
+++ s.sh
@@ -1,14 +1,14 @@
-disp_n=`hwinfo --monitor --short | grep "" --count`
-if [[ $disp_n -eq 2 ]]; then
-    export GDK_DPI_SCALE=1.2
-    export GDK_SCALE=1.2
-    #export QT_SCALE_FACTOR=1.4
-    export QT_SCALE_FACTOR=1
-else
+#disp_n=`hwinfo --monitor --short | grep "" --count`
+#if [[ $disp_n -eq 2 ]]; then
+    # export GDK_DPI_SCALE=1.2
+    # export GDK_SCALE=1.2
+    # export QT_SCALE_FACTOR=1.4
+#    export QT_SCALE_FACTOR=1
+# else
     export GDK_DPI_SCALE=1
     export GDK_SCALE=1
     export QT_SCALE_FACTOR=1
-fi
+# fi
 #export QT_AUTO_SCREEN_SCALE_FACTOR=1
 #export QT_WAYLAND_FORCE_DPI=physical
 #export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
